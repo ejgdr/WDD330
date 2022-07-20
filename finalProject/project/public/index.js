9170a66e07accc6eee03501707ced9da7a14ec0d
ejgdr <gui20006@byui.edu>
2022-07-19 23:06:40 -0600

    Finishing up and CSS

--- finalProject/project/public/index.js
+++ finalProject/project/public/index.js
@@ -5,28 +5,34 @@ if ('geolocation' in navigator) {
         const longitude = position.coords.longitude;
         document.getElementById('latitude').textContent = latitude.toFixed(2);
         document.getElementById('longitude').textContent = longitude.toFixed(2);
-
+      
         const api_url = `/earth/${latitude},${longitude}`;
+
         const response = await fetch(api_url);
         const json = await response.json();
         console.log(json);
         const location_pic = json.pic_location;
         const day_pic = json.pic_day;
         const notif = json.notifications;
-
+        
         document.getElementById('location_image').src = location_pic.url;
 
         document.getElementById('day_explanation').textContent = day_pic.explanation;
         document.getElementById('day_image').src = day_pic.url;
+        document.getElementById('day_hdImage').href = day_pic.hdurl;
+        document.getElementById('day_title').textContent = day_pic.title;
+        document.getElementById('day_copyright').textContent = day_pic.copyright;
 
         let ul = document.getElementById('notifications');
         ul.innerHTML = '';
         for (let i = 0; i < notif.length; i++) {
             let li = document.createElement('li');
             let a = document.createElement('a');
-            li.appendChild(document.createTextNode(`
-                ${notif[i].messageIssueTime} ~ <a href="${notif[i].messageURL}">${notif[i].messageType} -- ${notif[i].messageID}`));
-
+            
+            li.appendChild(document.createTextNode(`${notif[i].messageIssueTime} ~ `));
+            a.appendChild(document.createTextNode(`${notif[i].messageType} -- ${notif[i].messageID}`));
+            a.setAttribute("href", `${notif[i].messageURL}`);
+            li.appendChild(a);
             ul.appendChild(li);
         }
     });
